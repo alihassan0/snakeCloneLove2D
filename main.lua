b3eed468c053facb05dfb05c055f92ce5ff50b24
ali hassan <alithephantom@gmail.com>
2016-07-08 14:00:33 +0200

    Draw snake

--- main.lua
+++ main.lua
@@ -1,8 +1,7 @@
-gridWidth = 10
-gridHeight = 10
-grid = {} -- grid 
-snake = {}
-snakeInitialLength = 3
+grid = {width=10, height = 10, array={}} -- grid 
+
+snake = {x=1, y=1, initialLength=3, array={}, direction={x=1, y=0}}
+
 
 num = 0
 
@@ -14,6 +13,7 @@ function love.draw()
     love.graphics.print("Hello World", 400, 300)
     love.graphics.print(".", num, 300)
     drawGrid()
+    drawSnake()
 
 end
 
@@ -26,25 +26,44 @@ function love.update(dt)
 end
 
 function init()
-	for i = 1, gridWidth do
-	    grid[i] = {}
+	---------------------- init grid -------------------------
+	for i = 1, grid.width do
+	    grid.array[i] = {}
 
-	    for j = 1, gridHeight do
-	        grid[i][j] = 0 -- Fill the values here
+	    for j = 1, grid.height do
+	        grid.array[i][j] = 0 -- Fill the values here
 	    end
+	end
+
+	---------------------- init snake -------------------------
+	for i = 1, snake.initialLength do
+	    snake.array[i] = {x=snake.x + i*snake.direction.x ,y= snake.y + i*snake.direction.y}
 	end
 
 end 
 
-function drawGrid()
-	for i = 1, #grid do
+function drawSnake()
+	for i=1, #snake.array do
+		drawCell(snake.array[i].x, snake.array[i].y, {r=0, g=255, b=0})
+	end
+end 
 
-	    for j = 1, #grid[i] do
+function drawCell(x, y, color)
+	print(x,y)
+	drawTriangle(5 + 50*x, 5+50*y, 40, 40, color)
+end 
+
+function drawGrid()
+	for i = 1, #grid.array do
+
+	    for j = 1, #grid.array[i] do
 	        drawTriangle(5 + 50*i, 5+50*j, 40, 40)
 	    end
 	end
 end 
 
-function drawTriangle(x, y, width, height)
+function drawTriangle(x, y, width, height, color)
+	color = color or {r=255, g=255, b=255}
+	love.graphics.setColor(color.r,color.g,color.b, 255)
 	love.graphics.rectangle( "fill", x, y, width, height, 0, 0)
 end 
