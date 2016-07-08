4908103955ba0c21903d75be6eeab806f27db8a1
ali hassan <alithephantom@gmail.com>
2016-07-08 16:44:19 +0200

    Add score

--- main.lua
+++ main.lua
@@ -1,18 +1,19 @@
-grid = {width=10, height = 10, array={}} -- grid 
+grid = {startPos={x=5, y=20},width=16, height = 13, tilesize=20, array={}} -- grid 
 
-snake = {x=1, y=1, initialLength=3, array={}, direction={x=1, y=0}, speed=2}
+snake = {x=1, y=1, initialLength=3, array={},lastDirection={x=1, y=0}, direction={x=1, y=0}, speed=8}
 
 food = {x=1,y=1}
 
 age = 0
+score = 0
 
 function love.load()
+	love.window.setMode( 330, 400 )
    init()
 end
 
 function love.draw()
-    love.graphics.print("Hello World", 400, 300)
-    love.graphics.print(".", num, 300)
+    love.graphics.print("Score :"..score, 10, 10)
     drawGrid()
     drawCell(food.x, food.y, {r=255, g=0, b=0})-- draw food
     drawSnake()
@@ -23,9 +24,8 @@ function love.update(dt)
 		updateSnake()
 		age = 0
 	end
-   	-- love.timer.sleep(1/fps)
    	age = age +dt
-   	if snake.direction.y == 0 then
+   	if snake.lastDirection.y == 0 then
 	   	if love.keyboard.isDown( "down" ) then
 	   		snake.direction = {x=0, y=1}
 	   	end
@@ -35,7 +35,7 @@ function love.update(dt)
 	   	end
 	end
 	
-   	if snake.direction.x == 0 then
+   	if snake.lastDirection.x == 0 then
 		if love.keyboard.isDown( "left" ) then
 	   		snake.direction = {x=-1, y=0}
 	   	end
@@ -62,6 +62,8 @@ function init()
 	    snake.array[i] = {x=1+(snake.x + snake.direction.x*i-1)%(grid.width),
 	    				y=1+(snake.y + snake.direction.y*i-1)%(grid.height)}
 	end
+
+	spawnFood()
 end 
 
 function drawSnake()
@@ -71,6 +73,8 @@ function drawSnake()
 end 
 
 function updateSnake()
+	snake.lastDirection.x = snake.direction.x
+	snake.lastDirection.y = snake.direction.y
 	for i = 1, #snake.array-1 do
 	    snake.array[i].x = snake.array[i+1].x
 	    snake.array[i].y = snake.array[i+1].y
@@ -82,27 +86,30 @@ function updateSnake()
 	if snake.array[#snake.array].x == food.x and snake.array[#snake.array].y == food.y then
 		newPart = {x=snake.array[1].x + (snake.array[2].x - snake.array[1].x),y= snake.array[1].y  + (snake.array[2].y - snake.array[1].y)}
 		table.insert(snake.array, 1, newPart)
-									 
-		for i=1, #snake.array do
-			print(i, snake.array[i].x, snake.array[i].y)
-		end
+		spawnFood()
+		score = score +1
 	end
 end 
 
-function drawCell(x, y, color)
-	drawTriangle(5 + 50*x, 5+50*y, 40, 40, color)
-end 
 
 function drawGrid()
 	for i = 1, #grid.array do
 
 	    for j = 1, #grid.array[i] do
-	        drawTriangle(5 + 50*i, 5+50*j, 40, 40)
+	        drawCell(i, j)
 	    end
 	end
 end 
 
-function drawTriangle(x, y, width, height, color)
+function drawCell(x, y, color)
+	drawRectangle(grid.startPos.x + grid.tilesize*.1 + grid.tilesize*(x-1), grid.startPos.y + grid.tilesize*.1+grid.tilesize*(y-1), grid.tilesize*.8, grid.tilesize*.8, color)
+end 
+
+function spawnFood ()
+	food.x = 1 + math.floor(love.math.random()*10)
+	food.y = 1 + math.floor(love.math.random()*10)
+end  
+function drawRectangle(x, y, width, height, color)
 	color = color or {r=255, g=255, b=255}
 	love.graphics.setColor(color.r,color.g,color.b, 255)
 	love.graphics.rectangle( "fill", x, y, width, height, 0, 0)
