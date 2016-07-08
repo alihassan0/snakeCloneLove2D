5bf203c3aced9f12456c23e991d488daf54e1adb
ali hassan <alithephantom@gmail.com>
2016-07-08 13:28:15 +0200

    Draw grid

--- main.lua
+++ main.lua
@@ -1,18 +1,20 @@
--- grid 
-grid = {}
-for i = 10, 10 do
-    grid[i] = {}
-
-    for j = 1, 5 do
-        grid[i][j] = 0 -- Fill the values here
-    end
-end
+gridWidth = 10
+gridHeight = 10
+grid = {} -- grid 
+snake = {}
+snakeInitialLength = 3
 
 num = 0
+
+function love.load()
+   init()
+end
+
 function love.draw()
     love.graphics.print("Hello World", 400, 300)
     love.graphics.print(".", num, 300)
-    drawTriangle(0, 0, 100, 100)
+    drawGrid()
+
 end
 
 function love.update(dt)
@@ -22,6 +24,26 @@ function love.update(dt)
       num = num + 10 * dt -- this would increment num by 100 per second
 
 end
+
+function init()
+	for i = 1, gridWidth do
+	    grid[i] = {}
+
+	    for j = 1, gridHeight do
+	        grid[i][j] = 0 -- Fill the values here
+	    end
+	end
+
+end 
+
+function drawGrid()
+	for i = 1, #grid do
+
+	    for j = 1, #grid[i] do
+	        drawTriangle(5 + 50*i, 5+50*j, 40, 40)
+	    end
+	end
+end 
 
 function drawTriangle(x, y, width, height)
 	love.graphics.rectangle( "fill", x, y, width, height, 0, 0)
